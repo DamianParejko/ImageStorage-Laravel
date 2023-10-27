f543d8a56afb855a5a0228d5a4cf837249a778c2
alenumerek <damian.parejko@gmail.com>
2023-10-27 14:38:02 +0200

    Add custom return response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,27 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function response($data = null, $code = 200, $message = null){
+
+        switch ($code) {
+            case 201:
+                $message = 'Created successfully';
+                break;
+            case 404:
+                $data = null;
+                $message = 'Not Found';
+                break;
+            case 422:
+                $data = null;
+            default:
+                break;
+        }
+
+        return response()->json([
+            'data' => $data,
+            'code' => $code,
+            'message' => $message
+        ], $code);
+    }
 }
