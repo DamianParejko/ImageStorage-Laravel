6ae434a9ac7cd403d06695ac7180e613d3a890bc
alenumerek <damian.parejko@gmail.com>
2023-10-29 23:59:51 +0100

    Add images in route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\ImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(ImageController::class)->group(function (){
+    Route::prefix('/images')->group(function () {
+        Route::get('/', 'temperature');
+        Route::post('/', 'upload');
+        Route::get('/download/{type}/{date}/{name}', 'download');
+        Route::delete('/{id}', 'delete');
+    });
 });
